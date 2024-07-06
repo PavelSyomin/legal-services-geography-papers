22fc12230d4c2f272053e16a19917902539aa1bb
Pavel Syomin <pas.ntg@gmail.com>
2024-07-06 13:37:43 +0500

    Edit according to reviewer's notes: add custom definition of law business in methods, use per capita counts in Fig. 1 instead of absolute ones, #1

--- law-firms-geography/Script.R
+++ law-firms-geography/Script.R
@@ -7,6 +7,7 @@ library(tidyr)
 courts <- read_csv("courts.csv")
 data <- read_csv("../../ru-smb-companies/legal/panel.csv")
 empl <- read_csv("empl.csv")
+pop <- read_csv("population.csv")
 tiles <- read_csv("russia-tiles.csv")
 
 # Common preparations
@@ -20,13 +21,20 @@ courts_by_region <- count(courts, region, name = "n_courts") %>%
 empl <- pivot_longer(
   empl, "2016":"2021", names_to = "year", values_to = "empl_total") %>% 
   mutate(year = as.numeric(year))
+pop <- pivot_longer(
+  pop, "2016":"2021", names_to = "year", values_to = "pop_total") %>% 
+  mutate(year = as.numeric(year))
 
-# Count of legal firms by region
-counts_map <- count(data, region, year) %>% 
+# Relative count of law firms by region
+counts_map_data <- count(data, region, year) %>% 
+  left_join(pop) %>% 
+  mutate(share = 1e5 * n / pop_total) %>% 
   group_by(region) %>% 
-  summarise(n = median(n)) %>% 
-  right_join(tiles, by = c("region" = "name")) %>% 
-  ggplot(aes(x = col, y = -row, fill = n)) +
+  summarise(share = median(share)) %>% 
+  right_join(tiles, by = c("region" = "name"))
+counts_map_breaks <- quantile(counts_map_data$share, seq(0, 1, .2), na.rm = TRUE)
+counts_map <- counts_map_data %>% 
+  ggplot(aes(x = col, y = -row, fill = share)) +
   geom_raster() +
   geom_text(
     aes(x = col - .4, y = -row + .3, label = code_en), 
@@ -34,28 +42,29 @@ counts_map <- count(data, region, year) %>%
     size = 3,
     hjust = 0) +
   geom_text(
-    aes(x = col - .4, y = -row - .3, label = round(n, 0)), 
+    aes(x = col - .4, y = -row - .3, label = round(share, 1)), 
     family = "Times New Roman", 
     size = 3,
     color = "gray20",
     hjust = 0,
     na.rm = TRUE) +
   scale_fill_binned(
-    name = "Median count of law firms (2016–21)\nNumbers on the map are exact values\nGrey fill is no data",
-    transform = "log10",
+    name = "Median number of law firms\nper 100,000 people (2016–21)\nScale breaks are 0.2-spaced quantiles\nNumbers on the map are exact values\nGrey fill is no data",
+    breaks = counts_map_breaks,
+    labels = round(counts_map_breaks, 0),
     high = "#eff3ff", low = "#2171b5"
   ) +
   coord_fixed(expand = FALSE) +
   theme_void(base_family = "Times New Roman", base_size = 11) +
   theme(
-    legend.position = c(.9, .05),
+    legend.position = c(.9, .01),
     legend.justification = c(1, 0),
     legend.direction = "horizontal",
     legend.title.position = "top"
   )
 counts_map
 
-# Shares of companies among legal firms
+# Shares of companies among law firms
 shares_map <- count(data, region, year, kind) %>% 
   group_by(region, year) %>%
   mutate(share = n / sum(n)) %>% 
@@ -131,7 +140,7 @@ employees_map <- data %>%
     name = "Share of region's employees", 
     n.breaks = 4) +
   scale_fill_binned(
-    name = "Share of country's legal workers", transform = "log10",
+    name = "Share of country's lawyers", transform = "log10",
     high = "#eff3ff", low = "#2171b5",
     labels = function(x) round(x, digits = 2)) +
   coord_fixed(expand = FALSE) +

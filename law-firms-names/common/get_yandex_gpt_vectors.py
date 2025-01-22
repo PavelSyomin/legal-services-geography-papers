8fd0e021b1d641aefd1d75dfcf6536515685ccda
Pavel Syomin <pas.ntg@gmail.com>
2025-01-22 16:49:13 +0500

    Improve script to get text vectors: retrieve folder id and oauth tokens from env vars, get iam token automatically, #4

--- law-firms-names/common/get_yandex_gpt_vectors.py
+++ law-firms-names/common/get_yandex_gpt_vectors.py
@@ -1,4 +1,5 @@
 import csv
+import os
 import time
 
 import requests
@@ -6,7 +7,7 @@ import numpy as np
 import tqdm
 
 
-def get_embedding(text: str) -> np.array:
+def get_embedding(query_uri: str, embed_url: str, text: str) -> np.array:
     query_data = {
         "modelUri": query_uri,
         "text": text,
@@ -17,19 +18,35 @@ def get_embedding(text: str) -> np.array:
     )
 
 
-FOLDER_ID = ""
-IAM_TOKEN = ""
+def get_iam_token(oauth_token: str) -> str:
+    resp = requests.post(
+        "https://iam.api.cloud.yandex.net/iam/v1/tokens",
+        data=str({'yandexPassportOauthToken': oauth_token})
+    )
 
-query_uri = f"emb://{FOLDER_ID}/text-search-query/latest"
+    if not resp.ok:
+        raise RuntimeError("Failed to get IAM token for Yandex Cloud")
+
+    return resp.json()["iamToken"]
+
+
+folder_id = os.environ.get("FOLDER_ID")
+oauth_token = os.environ.get("OAUTH_TOKEN")
+
+if not (folder_id and oauth_token):
+    raise RuntimeError("FOLDER_ID and OAUTH_TOKEN env vars must be set")
+
+iam_token = get_iam_token(oauth_token)
+query_uri = f"emb://{folder_id}/text-search-query/latest"
 embed_url = "https://llm.api.cloud.yandex.net:443/foundationModels/v1/textEmbedding"
-headers = {"Content-Type": "application/json", "Authorization": f"Bearer {IAM_TOKEN}", "x-folder-id": f"{FOLDER_ID}"}
+headers = {"Content-Type": "application/json", "Authorization": f"Bearer {iam_token}", "x-folder-id": f"{folder_id}"}
 
-with open("names.csv") as f:
+with open("lemmas.csv") as f:
     reader = csv.DictReader(f)
     names = [row["name"] for row in reader]
 
 missing = []
-with open("names-vectors.csv", "w") as f:
+with open("lemmas-vectors.csv", "w") as f:
     fieldnames = ["name"] + [f"dim_{i}" for i in range(256)]
     writer = csv.DictWriter(f, fieldnames=fieldnames)
 
@@ -37,8 +54,8 @@ with open("names-vectors.csv", "w") as f:
 
     for name in tqdm.tqdm(names):
         try:
-            emb = get_embedding(name)
-        except:
+            emb = get_embedding(query_uri, embed_url, name)
+        except Exception:
             missing.append(name)
 
         row = dict(zip(fieldnames, [name] + list(emb)))
